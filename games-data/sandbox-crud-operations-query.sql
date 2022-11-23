d576715ee1f2fbd733e67119795cac8c1e65dbd9
Madeline Chavero <Madeline.Chavero01@utrgv.edu>
2022-11-22 23:33:30 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES (78497898, 1, 'Hello!');
+
+--UPDATE sandbox SET double_value = -2.1543054 WHERE id=165;
+
+--DELETE FROM sandbox WHERE id=99;
+
+SELECT * FROM sandbox;
